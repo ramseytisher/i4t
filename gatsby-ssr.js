3575b79681316c0c724160876ea8aee4990f2096
rt022748 <ramsey.tisher@cerner.com>
2021-02-09 16:34:50 -0500

    latest

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-// import { MdsProvider } from "./src/context/mds-context"
-// import { UserProvider } from "./src/context/user-context"
-
-export const wrapRootElement = ({ element }) => (
-    <div>{element}</div>
-)
